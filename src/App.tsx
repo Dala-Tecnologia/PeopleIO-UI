2ce59a326daa570bb7701c17c1a86956e0d3e5d7
Denner <dennermpb@hotmail.com>
2025-11-08 00:32:45 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { FormularioColaborador } from './Components/Form/FormularioColaborador'
+
+function App() {
+
+
+  return (
+    <>
+      <FormularioColaborador />
+    </>
+  )
+}
+
+export default App
